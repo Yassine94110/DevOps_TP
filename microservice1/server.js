d91e4dc2a47e5f8db0b87d3fcce02f263ddeedb4
Yassine OUHMMOU <yassine@cashflowpositif.fr>
2023-12-21 13:15:17 +0100

    init

---
+++ microservice1/server.js
@@ -0,0 +1,12 @@
+// server.js
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello from Microservice 1');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Microservice 1 listening on port ${PORT}`);
+});
---
+++ microservice2/server.js
@@ -0,0 +1,12 @@
+// server.js
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello from Microservice 2');
+});
+
+const PORT = process.env.PORT || 3001; // Notez le port différent
+app.listen(PORT, () => {
+  console.log(`Microservice 2 listening on port ${PORT}`);
+});
